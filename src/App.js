12fc1f2b4f0f30dea2bc80b16013bfd351f0071e
YooInSik <yu_in_s@naver.com>
2023-03-22 23:50:11 +0900

    2023.03.22 코딩애플 리액트 강의 2강 3강

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+
+  let [title,b] = useState('남자 코트 추천')
+
   return (
     <div className="App">
-      <h>Hello World!</h>
+      <div className="black-nav">
+        <h4>React Blog</h4>
+        </div>
+        <div className="list">
+          <h4>글 제목</h4>
+          <p>ㅇ월 ㅇ일 발행 </p> </div>
+          <div className="list">
+          <h4>글 제목</h4>
+          <p>ㅇ월 ㅇ일 발행 </p> </div>
+          <div className="list">
+          <h4>글 제목</h4>
+          <p>ㅇ월 ㅇ일 발행 </p> </div>
     </div>
   );
-}
+} 
 
 export default App;
