743c045bdc6ee814bd4b60efbdcece3e859478d2
YooInSik <yu_in_s@naver.com>
2023-04-05 00:19:23 +0900

    button onClick 기능 넣기 & state array or object type 일때, 변경

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+/* eslint-disable */
+
 import './App.css';
 import React, { useState } from 'react';
 
 function App() {
 
-  let [title,b] = useState('남자 코트 추천')
+  let [title,setTitle] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬독학'])
+  //javascript에서 array,object type은 reference type data
+
+  const [like ,setLike] = useState(0) 
+  const uplike = () => {
+    setLike(like+1);
+  }
+  const sortTitle = () =>{
+    let copy = [...title]
+
+    setTitle(copy.sort())
+  }
 
   return (
     <div className="App">
@@ -11,13 +24,14 @@ function App() {
         <h4>React Blog</h4>
         </div>
         <div className="list">
-          <h4>글 제목</h4>
+          <button onClick={sortTitle}>Sort Title</button>
+          <h4>{title[0]}<span onClick={uplike}>👍</span>{like}</h4>
           <p>ㅇ월 ㅇ일 발행 </p> </div>
           <div className="list">
-          <h4>글 제목</h4>
+          <h4>{title[1]}<span>👍</span>0</h4>
           <p>ㅇ월 ㅇ일 발행 </p> </div>
           <div className="list">
-          <h4>글 제목</h4>
+          <h4>{title[2]}<span>👍</span>0 </h4>
           <p>ㅇ월 ㅇ일 발행 </p> </div>
     </div>
   );
